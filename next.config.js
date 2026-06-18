6da4facd25500f28d53c62f9ee427dbaf3de4fbf
mednabb <m.elaannab@gmail.com>
2026-06-18 14:19:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: { unoptimized: true },
+};
 
 module.exports = nextConfig;
